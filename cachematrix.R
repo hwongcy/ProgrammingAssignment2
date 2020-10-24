e9c14f123f11042da698091e8c80451c24e54c51
hwongcy <hwongcy@gmail.com>
2020-10-24 09:37:23 +0800

    More descriptions of the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -3,30 +3,40 @@
 
 ## Write a short comment describing this function
 
+## This function used to accept a matrix as an input and 
+## cache the inverse of the matrix. 
+
+
 makeCacheMatrix <- function(x = matrix()) {
     
-    m <- NULL
+    i <- NULL
     
     # set the value of the matrix
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        i <<- NULL
     }
     
-    # get the value of the matrix
-    get <- function() x
+    # get the matrix
+    get <- function() {
+        x
+    }
     
-    # set the value of the inverse
-    setinv <- function(inv) m <<- inv
+    # cache the inverse
+    setinverse <- function(inverse) {
+        i <<- inverse
+    }
     
-    # get the value of the inverse
-    getinv <- function() m
+    # get the inverse from cache
+    getinverse <- function() {
+        i
+    }
     
     list(
         set = set,
         get = get,
-        setinv = setinv,
-        getinv = getinv
+        setinverse = setinverse,
+        getinverse = getinverse
     )
     
 }
@@ -34,23 +44,29 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+## This function is used to 
+## 1. check whether the inverse is cached. If it is not cached,
+##    it will compute the inverse of the matrix and
+##    cache the inverse.
+## 2. if the inverse is cached, it will return the cached inverse.
+
 cacheSolve <- function(x, ...) {
     
-    m <- x$getinv()
+    inverseMatrix <- x$getinverse()
     
     # check whether the inverse is cached.
-    if(!is.null(m)) {
+    if(!is.null(inverseMatrix)) {
         
         # inverse is cached, print the message and return the inverse.
         message("inverse matrix cached")
-        return(m)
+        return(inverseMatrix)
     }
     
     # inverse is not cached.
     z <- x$get()
-    m <- solve(z, ...) # find the inverse.
-    x$setinv(m) # cache the inverse.
+    inverseMatrix <- solve(z, ...) # find the inverse.
+    x$setinverse(inverseMatrix) # cache the inverse.
     
-    m # return the inverse.
+    inverseMatrix # return the inverse.
     
 }
